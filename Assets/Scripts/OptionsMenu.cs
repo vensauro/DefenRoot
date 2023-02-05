87d55c60f2c3e44a628f64e6c013510f9e0c716a
CainanNeves <cainan.lima@ufba.br>
2023-02-05 15:41:07 -0300

    adiciona cena de menu e menu de pausa

---
+++ Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+  public AudioMixer audioMixer;
+  public void setVolume(float volume)
+    {
+    audioMixer.SetFloat("volume", volume);
+  }
+}
